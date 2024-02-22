14c52936df032a15a4fa5924480fd6b51ca9af61
amirtophel <81131156+amirtophel@users.noreply.github.com>
2024-02-22 13:15:10 +1100

    Add files via upload

---
+++ chatgpt_deploy.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 11:15:00 2024
+
+@author: atop0005
+"""
+
+#Create a new virual environment
+#install the below libraries
+#pip install chromadb openai langchain_openai langchain_community langchain pypdf rapidocr-onnxruntime
+#put your API Key in constants.py
+#create a folder called data
+import sys
+import os
+import openai
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.llms import OpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.prompts import PromptTemplate
+import streamlit as st
+
+import constants
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
+
+documents = []
+for file in os.listdir("data"):
+    if file.endswith(".pdf"):
+        pdf_path = "./data/" + file
+        loader = PyPDFLoader(pdf_path)
+        documents.extend(loader.load())
+    elif file.endswith('.docx') or file.endswith('.doc'):
+        doc_path = "./data/" + file
+        loader = Docx2txtLoader(doc_path)
+        documents.extend(loader.load())
+    elif file.endswith('.txt'):
+        text_path = "./data/" + file
+        loader = TextLoader(text_path)
+        documents.extend(loader.load())
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+documents = text_splitter.split_documents(documents)
+
+vectordb = Chroma.from_documents(documents, embedding=OpenAIEmbeddings(), persist_directory="./data")
+vectordb.persist()
+
+pdf_qa = ConversationalRetrievalChain.from_llm(
+    ChatOpenAI(temperature=0.1, model_name="gpt-4-1106-preview"),
+    vectordb.as_retriever(search_kwargs={'k': 6}),
+    return_source_documents=True,
+    verbose=False
+)
+
+yellow = "\033[0;33m"
+green = "\033[0;32m"
+white = "\033[0;39m"
+
+chat_history = []
+print(f"{yellow}---------------------------------------------------------------------------------")
+print('Welcome to the ChatBot, Type your query below, to exit type exit, quit, q or f')
+print('---------------------------------------------------------------------------------')
+while True:
+    query = input(f"{green}Prompt: ")
+    if query == "exit" or query == "quit" or query == "q" or query == "f":
+        print('Exiting')
+        sys.exit()
+    if query == '':
+        continue
+    result = pdf_qa.invoke(
+        {"question": query, "chat_history": chat_history})
+    print(f"{white}Answer: " + result["answer"])
+    chat_history.append((query, result["answer"]))
+    
+    # Keep only the last 5 entries in chat_history
+    if len(chat_history) > 5:
+        chat_history = chat_history[-5:]
+
+def main():
+    st.set_page_config(
+        page_title="Geotechnical Engineering Tutor", page_icon=":bird:")
+
+    st.header("Answer :bird:")
+    message = st.text_area("Question")
+
+    if message:
+        st.write("Thinking...")
+
+        result = generate_response(message)
+
+        st.info(result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
